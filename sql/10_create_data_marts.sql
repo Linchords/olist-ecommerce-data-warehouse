37cbb33ced588d9d039f9189b5cae2c5e5c0f1cf
Lincoln M. Bayusie <Lincolnbedell80@gmail.com>
2026-09-17 15:41:58 +0000

    Create product performance data mart

--- sql/10_create_data_marts.sql
+++ sql/10_create_data_marts.sql
@@ -45,3 +45,56 @@ SELECT
 FROM marts.vw_monthly_sales
 ORDER by product_sales DESC 
 LIMIT 10;
+
+
+-- =========================================================
+-- PRODUCT PERFORMANCE MART
+-- Purpose:
+-- Summarizes product and category sales performance
+-- =========================================================
+
+CREATE OR REPLACE VIEW marts.vw_product_performance AS 
+
+SELECT 
+    dp.product_key,
+    dp.product_id,
+    dp.product_category_name,
+    dp.product_category_name_english,
+
+    COUNT(*) AS total_items_sold,
+    COUNT(DISTINCT fs.order_id) AS total_orders,
+
+    SUM(fs.price) AS total_product_sales,
+    SUM(fs.freight_value) AS total_freight_value,
+    SUM(fs.price + fs.freight_value) AS total_revenue,
+
+    AVG(fs.price) AS average_item_price
+
+FROM warehouse.fact_sales fs 
+
+INNER JOIN warehouse.dim_product dp 
+    ON fs.product_key = dp.product_key
+
+GROUP BY 
+    dp.product_key,
+    dp.product_id,
+    dp.product_category_name,
+    dp.product_category_name_english;
+
+-- Top Products by Sales
+SELECT 
+    product_id,
+    product_category_name_english,
+    total_product_sales
+FROM marts.vw_product_performance
+ORDER BY total_product_sales DESC
+LIMIT 10;
+
+-- Top Categories
+SELECT 
+    product_category_name_english,
+    SUM(total_product_sales) AS category_sales
+FROM marts.vw_product_performance
+GROUP BY product_category_name_english
+ORDER BY category_sales DESC
+LIMIT 10;
